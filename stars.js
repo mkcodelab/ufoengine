77bb4c30d3f0f3af9ef03e0cbac988f1dc6a217b
mikcodelab <michal.kulawik@gmail.com>
2021-06-17 08:46:39 +0200

    master

--- stars.js
+++ stars.js
@@ -6,10 +6,10 @@ function setSize() {
     canvas.height = window.innerHeight;
 }
 setSize();
-drawStars();
+// drawStars();
 window.addEventListener('resize', ()=>{
     setSize();
-    drawStars();
+    // drawStars();
 });
 
 function drawStars(){
@@ -25,3 +25,63 @@ function drawStars(){
         ctx.closePath();
     }
 }
+
+class Star {
+    constructor(x, y, size, color) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+    }
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 180);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+let colorArray = ['#FFFFFF', '#FFCCFF', '#DDFFDD'];
+let starsArray = [];
+for (let i = 0; i < 200; i++) {
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    let size = Math.random() + 0.1;
+    let color = colorArray[Math.floor(Math.random()*colorArray.length)];
+
+    starsArray.push(new Star(x, y, size, color))
+}
+
+const comet = {
+    x: 0,
+    y: 0,
+    dx: 10,
+    dy: 2,
+    size: 2
+}
+function drawComet(){
+    ctx.beginPath();
+    ctx.arc(comet.x, comet.y, comet.size, 0, Math.PI*2);
+    ctx.fill();
+    ctx.closePath();
+}
+function updateComet(){
+    if (comet.x > canvas.width) {
+        comet.x = -100;
+        comet.y = Math.random() * canvas.height;
+    }
+    comet.x += comet.dx;
+    comet.y += comet.dy;
+}
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawComet();
+    for (let i = 0; i < starsArray.length; i++) {
+        starsArray[i].draw();
+    }
+    updateComet();
+    requestAnimationFrame(animate)
+}
+animate();
